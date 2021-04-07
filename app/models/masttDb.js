bcc188f4753820bcd5622963e47a6f32628ca4aa
yyan8151 <safe233@outlook.com>
2021-04-07 14:44:19 +0800

    deleted unused files

--- app/models/masttDb.js
+++
@@ -1,8 +0,0 @@
-// Connecting Azure MongoDB
-
-const mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q%3D%3D@mastt-news-feed-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@mastt-news-feed-db@", function (err, client) {
-  client.close();
-});
-
-module.exports = mongoClient;
