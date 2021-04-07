40aeb5c0124e713ab9cad5a23149dcd47166c4c5
yyan8151 <safe233@outlook.com>
2021-04-07 16:42:19 +0800

    updated holiday component

--- controllers/holiday/scripts/htmlParser.py
+++ controllers/holiday/scripts/htmlParser.py
@@ -1,31 +1,40 @@
 # pip install beautifulsoup4
 from bs4 import BeautifulSoup
+import json
 #会读取调用该脚本的文件路径
-file = open('./controllers/holiday/holidayData/sduview.html',encoding='utf-8' )
-soup = BeautifulSoup(file, features="html.parser")
-
-tr_arr = soup.table.find_all("tr")
 
 list = []
+with open('./controllers/holiday/scripts/address.json', 'r', encoding='utf8') as f:
+    json_data = json.load(f)
+    for record in json_data:
+        country = record['country']
+        file = open('./controllers/holiday/holidayData/' + country +'.html',encoding='utf-8' )
+        soup = BeautifulSoup(file, features="html.parser")
 
-for tr in tr_arr:
-    td_arr = tr.find_all("td")
-    if(len(td_arr) != 0):
-        des = ''
-        if td_arr[4].string is None:
-            des = td_arr[3].string
-        else:
-            des = td_arr[3].string +" " + td_arr[4].string
-        data = {
-            "id": 1,
-            "title": td_arr[2].string,
-            "image": "a URL",
-            "start_date": td_arr[1].string,
-            "end_date": td_arr[1].string,
-            "catagory": "holiday",
-            "summary": des
-        }
-        list.append(data)
+        tr_arr = soup.table.find_all("tr")
+        for tr in tr_arr:
+            td_arr = tr.find_all("td")
+            if(len(td_arr) != 0):
+                des = ''
+                if td_arr[4].string is None:
+                    des = td_arr[3].string
+                else:
+                    des = td_arr[3].string +" " + td_arr[4].string
+                    data = {
+                        "id": 1,
+                        "title": country+ ": " + td_arr[2].string,
+                        "image": "a URL",
+                        "start_date": td_arr[1].string,
+                        "end_date": td_arr[1].string,
+                        "catagory": "holiday",
+                        "summary": des
+                        }
+                    list.append(data)
+
 print(list)
+
+
+
+
 
     
