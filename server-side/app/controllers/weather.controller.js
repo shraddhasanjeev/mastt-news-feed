c7ecf3242c3b47e99c6dd8443cbc5642ddefbac6
Lakshesh Girdhar <lgir7750@uni.sydney.edu.au>
2021-04-29 18:57:41 +1000

    weather api data loop

--- server-side/app/controllers/weather.controller.js
+++ server-side/app/controllers/weather.controller.js
@@ -1,13 +1,20 @@
 const request = require('request');
 const config = require('../config.json');
+const util = require('util')
+
+
 
 module.exports.fetchWeather = function(req,res){
 
-    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + config.locationCoordinates.hyderabad.latitude + "&lon=" + config.locationCoordinates.hyderabad.longitude + "&exclude=current,minutely,hourly,daily&appid=" + config.tokens.openweatherapi;
-    
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-        console.log(body)
-        }
-    })
+    config.locationCoordinates.forEach(city => {
+
+        var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + city.latitude + "&lon=" + city.longitude + "&exclude=current,minutely,hourly,daily&appid=" + config.tokens.openweatherapi;
+
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+            console.log(body);
+            res.status(200);
+            }
+        })
+    });
 }
