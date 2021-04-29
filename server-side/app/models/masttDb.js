05947c739ae79862946a66997b29e5eeb70fc582
Shraddha Sanjeev <ssan9671@uni.sydney.edu.au>
2021-04-30 01:48:27 +1000

    localhost mongo connectivity

--- server-side/app/models/masttDb.js
+++ server-side/app/models/masttDb.js
@@ -1,30 +1,28 @@
 // Connecting Azure MongoDB
 
-/*
-const mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q%3D%3D@mastt-news-feed-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@mastt-news-feed-db@", function (err, client) {
-  client.close();
-});
-
-module.exports = mongoClient;*/
-
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-//const mongoUrl = "mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q==@mastt-news-feed-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mastt-news-feed-db@"
-const mongoUrl = "mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q==@mastt-news-feed-db.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false"
 
+//const mongoUrl = "mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q==@mastt-news-feed-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mastt-news-feed-db@"
+// const mongoUrl = "mongodb://mastt-news-feed-db:yhtVbOS7558rETbRlA0rHGRy4WJ2MxiEsnsPZoWEs5y4AMJ7MO4MFzMuwARO9l1scMHdnwmRXFSVmS1YBX3S2Q==@mastt-news-feed-db.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false"
+
+// function connect() {
+//   return mongoose.connect(mongoUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   },  function () {
+//     console.log('mongodb connected')
+//   });
+// }
 
 function connect() {
-  return mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  },  function () {
+  mongoose.connect('mongodb://localhost/masttDB', { useNewUrlParser: true }, function () {
     console.log('mongodb connected')
   });
 }
+
 module.exports = {
   mongoose,
   connect
