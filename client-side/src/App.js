ef83f3f1fb42c57945140195a06f6467b9425715
Weiqiang Li <weli6221@sydney.uni.edu.au>
2021-04-30 12:00:06 +1000

    added new animation for weather loading and a placeholder for holiday display

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,13 +1,25 @@
-import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import HolidayLogic from './components/HolidayLogic';
 import WeatherLogic from './components/WeatherLogic';
 
 function App() {
   return (
-    <div className="App">
-      <WeatherLogic defaultLocation="Sydney, AU" />
-      <WeatherLogic defaultLocation="Sydney, AU" />
-    </div>
+    <>
+      <div className="card">
+        <WeatherLogic defaultLocation="Sydney, AU" />
+        <WeatherLogic defaultLocation="Brisbane, AU" />
+        <WeatherLogic defaultLocation="Hyderabad, IN" />
+        <WeatherLogic defaultLocation="Manila, PH" />
+      </div>
+
+      <div className="note">
+        <HolidayLogic presetCity="Sydney" />
+        <HolidayLogic presetCity="Brisbane" />
+        <HolidayLogic presetCity="Hyderabad" />
+        <HolidayLogic presetCity="Manila" />
+      </div>
+    </>
   );
 }
 
