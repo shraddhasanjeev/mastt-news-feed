2ed9d139dcb1d563f8ebb988442138b8422d6a5d
yyan8151 <safe233@outlook.com>
2021-05-05 14:21:28 +0800

    connected holiday to DB

--- server-side/app/controllers/holiday/holidayService.js
+++ server-side/app/controllers/holiday/holidayService.js
@@ -1,6 +1,6 @@
 const { MessageQueue } = require("./MessageQueue");
 var messageQueue = new MessageQueue();
-
+var holidaySchema = require("../../models/holidaySchema");
 
 var thisYear = new Date().getFullYear()
 duration = (60*60*1000)*24 // 1 day
@@ -10,10 +10,11 @@ async function update() {
     if (r1 == "Valid") {
     } else if (r1 == "Updated") {
         await updateData()
+        await extractInfo()
     } else {
         throw new Error("Failed to fetch last update date")
     }
-    await extractInfo()
+    
 }
 
 function checkForUpdate() {
@@ -63,75 +64,72 @@ function extractInfo() {
                 retrunedData.indexOf('(') + 1,
                 retrunedData.lastIndexOf(')')
             )).split('), (')
-            for (var index = 0; index < arr.length; index++) {
-                var temp = arr[index].split(",")
-                var month = -1
-                var day = 0
-                // ����try�������ݴ���
-                if (temp[2].length == 9) {
-                    switch (temp[2].substring(2, 5)) {
-                        case "Jan":
-                            month = 0
-                            break
-                        case "Feb":
-                            month = 1
-                            break
-                        case "Mar":
-                            month = 2
-                            break
-                        case "Apr":
-                            month = 3
-                            break
-                        case "May":
-                            month = 4
-                            break
-                        case "Jun":
-                            month = 5
-                            break
-                        case "Jul":
-                            month = 6
-                            break
-                        case "Aug":
-                            month = 7
-                            break
-                        case "Sep":
-                            month = 8
-                            break
-                        case "Oct":
-                            month = 9
-                            break
-                        case "Nov":
-                            month = 10
-                            break
-                        case "Dec":
-                            month = 11
-                            break
+            try {
+                for (var index = 0; index < arr.length; index++) {
+                    var temp = arr[index].split(",")
+                    var month = -1
+                    var day = 0
+                    if (temp[2].length == 9) {
+                        switch (temp[2].substring(2, 5)) {
+                            case "Jan":
+                                month = 0
+                                break
+                            case "Feb":
+                                month = 1
+                                break
+                            case "Mar":
+                                month = 2
+                                break
+                            case "Apr":
+                                month = 3
+                                break
+                            case "May":
+                                month = 4
+                                break
+                            case "Jun":
+                                month = 5
+                                break
+                            case "Jul":
+                                month = 6
+                                break
+                            case "Aug":
+                                month = 7
+                                break
+                            case "Sep":
+                                month = 8
+                                break
+                            case "Oct":
+                                month = 9
+                                break
+                            case "Nov":
+                                month = 10
+                                break
+                            case "Dec":
+                                month = 11
+                                break
+                        }
+                        day = parseInt(temp[2].substring(6, 8))
                     }
-                    day = parseInt(temp[2].substring(6, 8))
+
+                    var dateString1 = ""
+                    var dateString2 = ""
+                    if (month == -1) {
+                        dateString = "error"
+                    } else {
+                        dateString1 = new Date(thisYear, month, day).toDateString()
+                        dateString2 = new Date(thisYear, month, day + 1).toDateString()
+                    }
+                    var title = temp[0].replace(/^(\s|\')+|(\s|\')+$/g, '')
+                    var city = temp[1].replace(/\"/g, '').replace(/^(\s|\')+|(\s|\')+$/g, '')
+                    var content = temp[3].replace(/\"/g, '').replace("\\xa0", "").replace(/^(\s|\')+|(\s|\')+$/g, '')
+
+                    saveToDb(title, city, content, dateString1, dateString2)
+                    resolve("Updated")
                 }
+            } catch (err) {
 
-                var dateString1 = ""
-                var dateString2 = ""
-                if (month == -1) {
-                    dateString = "error"
-                } else {
-                    dateString1 = new Date(thisYear, month, day).toDateString()
-                    dateString2 = new Date(thisYear, month, day + 1).toDateString()
-                }
-
-
-                var str = "{"
-                    + "\"id\" :" + index + ","
-                    + "\"title\": \"" + temp[0] + ":" + temp[1].replace(/\"/g, '') + "\","
-                    + "\"image\":" + "\"A URL\","
-                    + "\"start_date\": \"" + dateString1 + "\","
-                    + "\"end_date\": \"" + dateString2 + "\","
-                    + "\"category\": \"holiday\","
-                    + "\"summary\": \"" + temp[3].replace(/\"/g, '').replace("\\xa0", "") + "\""
-                    + "}"
-                messageQueue.addMessage(JSON.parse(str))
-                resolve("Updated")
             }
+            
         });
 
         ls.stderr.on('data', (data) => {
@@ -141,6 +139,21 @@ function extractInfo() {
         ls.on('close', (code) => {
         });
     })
+}
+
+function saveToDb(title, city, content, startDate, endDate) {
+    var holidayItem = new holidaySchema({
+        title: title,
+        city: city,
+        content: content,
+        start_date: startDate,
+        end_date: endDate
+    })
+    holidayItem.save()
+        .catch(err => {
+            console.log(err)
+            errors.push(err)
+        });
 }
 
 module.exports.initialize = function () {
