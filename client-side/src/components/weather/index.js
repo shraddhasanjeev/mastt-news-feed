25db8861cd729441e7cfc8742dbfaafa81a6b089
Weiqiang Li <weli6221@uni.sydney.edu.au>
2021-06-07 18:04:37 +1000

    weather card adjusted
    added image to weather card, adjusted font size

--- client-side/src/components/weather/index.js
+++ client-side/src/components/weather/index.js
@@ -1,48 +1,51 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react';
-import { Image, Container, Card, Button } from 'semantic-ui-react'
-import './index.css'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Image, Container, Card, Button } from "semantic-ui-react";
+import "./index.css";
 
 export default function Weather() {
+  const [data, setData] = useState([]);
+  const [id, setId] = useState([]);
 
-    const [data, setData] = useState([])
-    const [id, setId] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      let cities = ["Buenos Aires", "Hyderabad", "Manilla", "Sydney"];
+      const responses = await Promise.all(
+        cities.map((city) =>
+          axios.get(
+            `https://mastt-news-feed-server.azurewebsites.net/getWeatherByCity?city=${city}&token=f39236376746483bb4f7922954f2f503`
+          )
+        )
+      );
+      setData(responses.map((res) => res.data[0]));
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            let cities = ["Buenos Aires","Hyderabad", "Manila", "Sydney"]
-            const responses = await Promise.all(
-                cities.map((city) => axios.get(`https://mastt-news-feed-server.azurewebsites.net/getWeatherByCity?city=${city}&token=f39236376746483bb4f7922954f2f503`))
-            )
-            setData(responses.map((res) => res.data[0]))
-        }
-        fetchData()
-    }, [])
+  return (
+    <Container style={{ marginTop: 100 }}>
+      <Card.Group itemsPerRow={4}>
+        {data.map((item) => {
+          return (
+            <Card className={id === item._id ? "card-item" : ""} key={item._id}>
+              <Image src={`${item.city}.jpg`} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>
+                  <h1>{item.city}</h1>
+                </Card.Header>
+                <Card.Description>
+                  <h3>{item.temperature} °C</h3>
+                </Card.Description>
+                <Card.Description>
+                  <h3>{item.outlook}</h3>
+                </Card.Description>
 
-
-    return (
-        <Container style={{ marginTop: 100 }} >
-            <Card.Group itemsPerRow={4}>
-                {
-                    data.map(item => {
-                        return <Card className={id === item._id ? "card-item" :""} key={item._id} >
-                            {/* <Image src={item.img} wrapped ui={false} /> */}
-                            <Card.Content onClick={()=> {setId(item._id)}}>
-                                <Card.Header>{item.city}</Card.Header>
-                                <Card.Description>
-                                    <h4>Temperature: {item.temperature}</h4>
-                                </Card.Description>
-                                <Card.Description>
-                                    <h4>outlook: {item.outlook}</h4>
-                                </Card.Description>
-                            </Card.Content>
-                            <Card.Content extra>
-                            </Card.Content>
-
-                        </Card>
-                    })
-                }
-            </Card.Group>
-        </Container>
-    )
+              </Card.Content>
+              <Card.Content extra></Card.Content>
+            </Card>
+          );
+        })}
+      </Card.Group>
+    </Container>
+  );
 }
