446775cf0c660cea188f5e17ec9b9344f299a392
Weiqiang Li <weli6221@uni.sydney.edu.au>
2021-06-10 13:19:45 +1000

    website name change

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,7 +3,6 @@ import Nav from "./components/nav";
 import News from "./components/news/index";
 import Holidays from "./components/holidays";
 import "./App.css";
-import { Item, Container, Label, Button } from "semantic-ui-react";
 
 export default function App() {
   return (
